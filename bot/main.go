7ed16ea09bbaac13faf9142419a66d8cf3948ba7
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-10-19 22:50:10 +0530

    feat: add auth

--- bot/main.go
+++ bot/main.go
@@ -1,75 +1,960 @@
 package main
 
 import (
-	"log"
-	"os"
-	"io"
-	"fmt"
-	// "os"
-	"os/exec"
-	// "strings"
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "os/exec"
+    "strings"
+    "time"
+    "github.com/joho/godotenv"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/joho/godotenv"
-	"bytes"
-
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func main() {
-
-	logFile, err := os.OpenFile("log/bot.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logFile.Close()
-
-	logger := log.New(io.MultiWriter(os.Stdout, logFile), "", log.LstdFlags)
-
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatal("Please add your api token in .env file with BOT_API_KEY=your_token")
-	}
-
-	botToken := os.Getenv("BOT_API_KEY")
-
-bot, err := tgbotapi.NewBotAPI(botToken)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		fmt.Print("$")
-		if update.Message != nil { // If we got a message
-			// log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		
-			// bot.Send(msg)
-			fmt.Println(update.Message.Text)
-			logger.Println(update.Message.Text)
-
-			cmd := exec.Command("sh", "-c", fmt.Sprintf("%s",update.Message.Text))
-			var out bytes.Buffer
-		  cmd.Stdout = &out
-
-		  cmd.Stderr = os.Stderr 
-		  err = cmd.Run()
-		  if err != nil {
-			   fmt.Println("invalid Command");
-		}
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, out.String())
-			msg.ReplyToMessageID = update.Message.MessageID
-
-				  bot.Send(msg)
-
-		}
-	}
+type Config struct {
+    BotToken     string   `json:"bot_token"`
+    AllowedUsers []int64  `json:"allowed_users"`
 }
+
+type Bot struct {
+    api            *tgbotapi.BotAPI
+    allowedUserIDs map[int64]bool
+    logFile        string
+}
+
+func NewBot(config Config, logFile string) (*Bot, error) {
+    api, err := tgbotapi.NewBotAPI(config.BotToken)
+    if err != nil {
+        return nil, fmt.Errorf("failed to create bot: %v", err)
+    }
+
+    allowedMap := make(map[int64]bool)
+    for _, userID := range config.AllowedUsers {
+        allowedMap[userID] = true
+    }
+
+    return &Bot{
+        api:            api,
+        allowedUserIDs: allowedMap,
+        logFile:        logFile,
+    }, nil
+}
+
+func (b *Bot) isAuthorized(userID int64) bool {
+    return b.allowedUserIDs[userID]
+    // return true
+}
+
+func (b *Bot) logCommand(userID int64, command string) error {
+    f, err := os.OpenFile(b.logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    defer f.Close()
+
+    logEntry := fmt.Sprintf("[%s] UserID: %d, Command: %s\n",
+        time.Now().Format(time.RFC3339),
+        userID,
+        command)
+
+    if _, err := f.WriteString(logEntry); err != nil {
+        return err
+    }
+    return nil
+}
+
+func (b *Bot) executeCommand(command string, sudo bool) (string, error) {
+    var cmd *exec.Cmd
+
+    if sudo {
+        cmd = exec.Command("sudo", append([]string{"-n"}, strings.Fields(command)...)...)
+    } else {
+        cmd = exec.Command("/bin/sh", "-c", command)
+    }
+
+    output, err := cmd.CombinedOutput()
+    if err != nil {
+        return string(output), fmt.Errorf("command execution failed: %v", err)
+    }
+    return string(output), nil
+}
+
+func (b *Bot) handleStart(message *tgbotapi.Message) {
+    response := "Welcome to the Remote Command Execution Bot!\n" +
+        "Available commands:\n" +
+        "/start - Show this help message\n" +
+        "/getlogs - Get command execution logs\n" +
+        "For regular commands, just type the command\n" +
+        "For sudo commands, prefix with 'sudo '\n\n" +
+        fmt.Sprintf("Your Telegram ID: %d", message.From.ID)
+
+    msg := tgbotapi.NewMessage(message.Chat.ID, response)
+    b.api.Send(msg)
+}
+
+func (b *Bot) handleGetLogs(message *tgbotapi.Message) {
+    logs, err := os.ReadFile(b.logFile)
+    if err != nil {
+        msg := tgbotapi.NewMessage(message.Chat.ID, "Failed to read logs")
+        b.api.Send(msg)
+        return
+    }
+
+    response := "Recent command logs:\n" + string(logs)
+    msg := tgbotapi.NewMessage(message.Chat.ID, response)
+    b.api.Send(msg)
+}
+
+func (b *Bot) handleCommand(message *tgbotapi.Message) {
+    command := message.Text
+    isSudo := strings.HasPrefix(command, "sudo ")
+
+    if isSudo {
+        command = strings.TrimPrefix(command, "sudo ")
+    }
+
+    // Check authorization before executing the command
+    if !b.isAuthorized(message.From.ID) {
+        msg := tgbotapi.NewMessage(message.Chat.ID, "Unauthorized access")
+        b.api.Send(msg)
+        return
+    }
+
+    // Log the command
+    if err := b.logCommand(message.From.ID, command); err != nil {
+        log.Printf("Failed to log command: %v", err)
+    }
+
+    // Execute the command
+    output, err := b.executeCommand(command, isSudo)
+    if err != nil {
+        msg := tgbotapi.NewMessage(message.Chat.ID, fmt.Sprintf("Error: %v\n%s", err, output))
+        b.api.Send(msg)
+        return
+    }
+
+    msg := tgbotapi.NewMessage(message.Chat.ID, output)
+    b.api.Send(msg)
+}
+
+func loadConfig(configFile string) (Config, error) {
+    file, err := os.Open(configFile)
+    if err != nil {
+        return Config{}, fmt.Errorf("failed to open config file: %v", err)
+    }
+    defer file.Close()
+
+    var config Config
+    decoder := json.NewDecoder(file)
+    if err := decoder.Decode(&config); err != nil {
+        return Config{}, fmt.Errorf("failed to parse config file: %v", err)
+    }
+
+    return config, nil
+}
+
+func main() {
+    godotenv.Load(".env")
+
+    // Load config from file
+    config, err := loadConfig("config.json")
+    if err != nil {
+        log.Fatalf("Error loading config: %v", err)
+    }
+
+    // Initialize the bot with your token and allowed user IDs from the config
+    bot, err := NewBot(config, "bot_commands.log")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    log.Printf("Bot started")
+
+    updateConfig := tgbotapi.NewUpdate(0)
+    updateConfig.Timeout = 30
+
+    updates := bot.api.GetUpdatesChan(updateConfig)
+
+    for update := range updates {
+        if update.Message == nil {
+            continue
+        }
+
+        // Log or show the user ID without restriction
+        log.Printf("Message from UserID: %d", update.Message.From.ID)
+
+        // Handle different types of commands
+        switch {
+        case update.Message.Command() == "start":
+            bot.handleStart(update.Message)
+        case update.Message.Command() == "getlogs":
+            bot.handleGetLogs(update.Message)
+        default:
+            bot.handleCommand(update.Message)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
