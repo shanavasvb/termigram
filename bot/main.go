596fe8cea02fd6f3086002abc18a59e44887d53b
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-10-19 19:44:46 +0530

    add basic remote shell functionality

---
+++ bot/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"log"
+	"os"
+	"io"
+	"fmt"
+	// "os"
+	"os/exec"
+	// "strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"bytes"
+
+)
+type pendingSudoCommand struct {
+	chatID   int64
+	command  string
+	password chan string
+	mutex    sync.Mutex
+}
+
+var pendingCommands = make(map[int64]*pendingSudoCommand)
+
+func main() {
+
+	logFile, err := os.OpenFile("log/bot.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
+	logger := log.New(io.MultiWriter(os.Stdout, logFile), "", log.LstdFlags)
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Please add your api token in .env file with BOT_API_KEY=your_token")
+	}
+
+	botToken := os.Getenv("BOT_API_KEY")
+
+bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		fmt.Print("$")
+		if update.Message != nil { // If we got a message
+			// log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		
+			// bot.Send(msg)
+			fmt.Println(update.Message.Text)
+			logger.Println(update.Message.Text)
+
+			cmd := exec.Command("sh", "-c", fmt.Sprintf("%s",update.Message.Text))
+			var out bytes.Buffer
+		  cmd.Stdout = &out
+
+		  cmd.Stderr = os.Stderr 
+		  err = cmd.Run()
+		  if err != nil {
+			   fmt.Println("invalid Command");
+		}
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, out.String())
+			msg.ReplyToMessageID = update.Message.MessageID
+
+				  bot.Send(msg)
+
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.22.5
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
